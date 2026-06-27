242a8039117b1c6d1202089fe5269b92b1316109
robbytrujillo <robbyikusuma88@gmail.com>
2026-06-27 07:18:39 +0700

    Introduction Graduation Attendance

--- admin/siswa.php
+++ admin/siswa.php
@@ -290,6 +290,8 @@ if ($keyword !== '') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Siswa | Absensi Wisuda</title>
 
+    <link rel="icon" type="image/png" href="../assets/img/logo.png">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
